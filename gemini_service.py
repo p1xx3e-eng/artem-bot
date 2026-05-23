453ab318dc7f658417824539ef1d412bf56db3cf
p1xx3e-eng <p1xx3e@gmail.com>
2026-05-24 02:02:41 +0700

    Add files via upload

--- gemini_service.py
+++ gemini_service.py
@@ -3,7 +3,7 @@ from groq import Groq
 from supabase_service import get_guide, get_posts
 
 client = Groq(api_key=os.environ["GROQ_API_KEY"])
-MODEL = "deepseek-r1-distill-llama-70b"
+MODEL = "mistral-saba-24b"
 
 def build_system_prompt() -> str:
     guide = get_guide()
